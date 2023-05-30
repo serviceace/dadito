00b6c7fc457e9e3cfdca6f7c7c651461bc701ecd
Gustavo Iky Robles <the.serviceace@gmail.com>
2023-05-30 14:40:35 -0300

    Add files via upload

--- code_dadito.js
+++ code_dadito.js
@@ -1,13 +1,52 @@
+// levantando elementos del DOM 
 const btnClick = document.querySelector('#btnClick');
 const numerito = document.querySelector('#numerito');
+const dadoUNO = document.querySelector('.dadoUNO');
 
-let numero = 0;
+
+//variables auxiliares
+let numero = 1;
+
+
+//eventos 
 
 btnClick.addEventListener('click', () => {
-	numero = Math.random()*10;
+	
+    numero = Math.random()*6;
     numero = Math.round(numero);
     console.log(numero);
-    numerito.textContent = numero;
+
+  switch (numero) {
+    case 1:
+      console.log('salio UNO');
+      dadazo.classList.add('uno');
+      break;
+    case 2:
+      console.log('salio DOS');
+      dadazo.classList.add('dos');
+      break;
+    case 3:
+      console.log('salio TRES');
+      dadazo.classList.add('tres');
+      break;
+    case 4:
+      console.log('salio CUATRO');
+      dadazo.classList.add('cuatro');
+      break;
+    case 5:
+      console.log('salio CINCO');
+      dadazo.classList.add('cinco');
+      break;
+    case 6:
+      console.log('salio SEIS');
+      dadazo.classList.add('seis')
+      break;
+    default:
+      console.log('salio CERO');
+  }
+
+ 
+    //dadazo.classList.add('seis')
 })
 
 
